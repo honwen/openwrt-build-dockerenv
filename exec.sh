a23c742be06ce6938d8edacda03d10342af3b35b
honwen.chan <honwen.chan@gmail.com>
2025-06-08 23:55:22 +0800

    cleanup

--- exec.sh
+++ exec.sh
@@ -6,5 +6,6 @@ WORKDIR=$(git rev-parse --show-toplevel)
 docker_home="/home/$(sed -n 's+^ARG *USER=++p' $WORKDIR/Dockerfile.builder)"
 
 touch ${WORKDIR}/workdir/.zshrc
+rm -f ${WORKDIR}/workdir/.zcompdump* ||:
 
 docker run --rm -i -t -v ${WORKDIR}:/data:ro -v ${WORKDIR}/workdir:$docker_home openwrt_builder:bookworm "$@"
