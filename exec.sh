d26fc60e6e008dd8539430546746b52c93d600f5
honwen.chan <honwen.chan@gmail.com>
2025-10-02 19:53:30 +0800

    bump ver to v24.10.3

--- exec.sh
+++ exec.sh
@@ -6,6 +6,6 @@ WORKDIR=$(git rev-parse --show-toplevel)
 docker_home="/home/$(sed -n 's+^ARG *USER=++p' $WORKDIR/Dockerfile.builder)"
 
 touch ${WORKDIR}/workdir/.zshrc
-rm -f ${WORKDIR}/workdir/.zcompdump* ||:
+rm -f ${WORKDIR}/workdir/.zcompdump* || :
 
-docker run --rm -i -t -v ${WORKDIR}:/data:ro -v ${WORKDIR}/workdir:$docker_home openwrt_builder:bookworm "$@"
+docker run --rm -i -t -v ${WORKDIR}:/data:ro -v ${WORKDIR}/workdir:$docker_home -w $docker_home/immortalwrt/latest openwrt_builder:bookworm "$@"
--- setup.sh
+++ setup.sh
@@ -10,13 +10,18 @@ docker build -t openwrt_builder:bookworm -f ${WORKDIR}/Dockerfile.builder .
 
 imwrt_root=${WORKDIR}/workdir/immortalwrt
 
-imwrt_24_ver='24.10.2'
+imwrt_24_ver='24.10.3'
 imwrt_24=$imwrt_root/$imwrt_24_ver
 
 mkdir -p $imwrt_root
 
+realpath $imwrt_root/latest | grep -qF "$imwrt_24_ver" || {
+  rm -f $imwrt_root/latest || :
+  ln -sf "$imwrt_24_ver" $imwrt_root/latest
+}
+
 git -C $imwrt_24 log 2>/dev/null | grep -qF "$imwrt_24_ver" || {
-  git -C $imwrt_root clone https://github.com/immortalwrt/immortalwrt -b openwrt-24.10 --depth 66 $imwrt_24_ver
+  git -C $imwrt_root clone https://github.com/immortalwrt/immortalwrt -b openwrt-24.10 --depth=66 $imwrt_24_ver
   git -C $imwrt_24 checkout v$imwrt_24_ver
 
   docker run --rm -i -t -v $(pwd)/workdir:$docker_home --workdir="$docker_home/immortalwrt/$imwrt_24_ver" openwrt_builder:bookworm ./scripts/feeds update -a
@@ -27,13 +32,13 @@ rm -rf $imwrt_24/files $imwrt_24/package/feeds/luci/luci-app-homeproxy $imwrt_24
 rm -rf $imwrt_24/package/feeds/packages/sing-box $imwrt_24/package/feeds/packages/xray-core $imwrt_24/package/feeds/packages/xray-plugin $imwrt_24/package/feeds/packages/v2raya || :
 
 [ -e $imwrt_24/package/feeds/openwrt-dnsmasq-extra ] ||
-  git -C $imwrt_24/package/feeds clone https://github.com/honwen/openwrt-dnsmasq-extra.git --depth 1
+  git -C $imwrt_24/package/feeds clone https://github.com/honwen/openwrt-dnsmasq-extra.git --depth=1
 
 [ -e $imwrt_24/package/feeds/openwrt-precompiled-feeds ] ||
-  git -C $imwrt_24/package/feeds clone https://github.com/honwen/openwrt-precompiled-feeds.git --depth 1
+  git -C $imwrt_24/package/feeds clone https://github.com/honwen/openwrt-precompiled-feeds.git --depth=1
 
 [ -e $imwrt_24/package/feeds/homeproxy ] ||
-  git -C $imwrt_24/package/feeds clone https://github.com/immortalwrt/homeproxy.git --depth 1
+  git -C $imwrt_24/package/feeds clone https://github.com/immortalwrt/homeproxy.git --branch=dev --depth=1
 
 cp -Rf ${WORKDIR}/extra/files $imwrt_24/
 cp -f ${WORKDIR}/extra/immortalwrt/.config $imwrt_24/
